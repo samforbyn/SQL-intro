ecb3dd4e42e95bd2c5850040491863afae284805
Sam Forbyn <samforbyn@gmail.com>
2021-12-06 18:00:05 -0700

    finished delete.sql

--- delete.sql
+++ delete.sql
@@ -8,3 +8,18 @@
 -- ('Nala', 'lion', 2),
 -- ('Marie', 'cat', 1),
 -- ('Flounder', 'fish', 8);
+
+
+did in SQL Tabs
+
+
+CREATE TABLE animals (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(47),
+    type VARCHAR(47), 
+    age INTEGER
+)
+SELECT * FROM animals
+DELETE FROM animals WHERE type = 'lion'
+DELETE FROM animals WHERE name LIKE 'M%'
+DELETE FROM animals WHERE age < 9
