900a257e51379925560dae4b8025051c6f8050d5
Sam Forbyn <samforbyn@gmail.com>
2021-12-06 17:40:16 -0700

    finished update.sql

--- update.sql
+++ update.sql
@@ -1,5 +1,5 @@
 UPDATE customer SET fax = null WHERE fax LIKE '%'
--- UPDATE customer SET company = 'Self' WHERE company LIKE null
+UPDATE customer SET company = 'Self' WHERE company IS null
 UPDATE customer SET last_name = 'Thompson' WHERE customer_id = 28
 UPDATE customer SET support_rep_id = 4 WHERE customer_id = 57
-UPDATE track SET composer = 'The darkness around us' WHERE genre_id = 3 AND composer = null
+UPDATE track SET composer = 'The darkness around us' WHERE genre_id = 3 AND composer IS null
